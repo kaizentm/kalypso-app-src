12b38c962c6ebb8de7e007500dc262b6ff0f0939
Eugene Fedorenko <eugene.fedor@gmail.com>
2025-03-21 12:31:03 -0700

    Update main.go

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>Hi there!</h1>")
+	fmt.Fprintf(w, "<h1>Hello there!</h1>")
 	fmt.Fprintf(w, "<div>I am working on <b>%s</b> cluster</div>", os.Getenv("CLUSTER_NAME"))
 	fmt.Fprintf(w, "<div>in <b>%s</b> region</div>", os.Getenv("REGION"))
 	fmt.Fprintf(w, "<div>at <b>%s</b> environment</div>", os.Getenv("ENVIRONMENT"))
